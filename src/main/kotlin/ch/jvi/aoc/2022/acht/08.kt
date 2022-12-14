d6e05463aff59f5317201ff97c07880974378589
Joshua Villing <joshua.villing@mimacom.com>
2022-12-15 00:16:00 +0100

    Add Day 8

---
+++ src/main/kotlin/ch/jvi/aoc/2022/acht/08.kt
@@ -0,0 +1,37 @@
+package ch.jvi.aoc.`2022`.acht
+
+import ch.jvi.aoc.Shame
+import ch.jvi.aoc.frame.loadData
+import java.util.function.Predicate
+
+fun main() {
+    val lines = loadData(8, 2022)
+//    puzzle1(lines)
+    puzzle2(lines)
+}
+
+fun puzzle1(lines: List<String>) {
+    val field = lines.map { it.map { c -> c.digitToInt() } }
+    val visibleTrees = mutableSetOf<Pair<Int, Int>>()
+    for (i in lines.first().indices) {
+        for (j in lines.indices) {
+            if (isVisible(i, j, field)) {
+                visibleTrees.add(Pair(i, j))
+            }
+        }
+    }
+    print(visibleTrees.size)
+}
+
+fun isVisible(i: Int, j: Int, field: List<List<Int>>): Boolean {
+    val treeHeight = field[i][j]
+
+    return field[i].drop(j + 1).all { it < treeHeight }                             // Right
+            || field[i].take(j).all { it < treeHeight }                              // Left
+            || (0 until i).all { field[it][j] < treeHeight }                    // Up
+            || (i + 1 until field.size).all { field[it][j] < treeHeight }      // Down
+}
+
+fun puzzle2(lines: List<String>) {
+
+}
